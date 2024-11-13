285fd9532623e976487c2c7c5f8f02eabbc1cf04
Nikola Rakocevic <nrakocevic@csuchico.edu>
2024-11-13 11:23:25 -0800

    leading characters tests

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -20,3 +20,44 @@ TEST(PasswordTest, single_letter_password)
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
 }
+
+TEST(PasswordTest, double_letter_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("BB");
+	ASSERT_EQ(2, actual);
+}
+
+TEST(PasswordTest, empty_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("");
+	ASSERT_EQ(0, actual);
+}
+
+TEST(PasswordTest, leading_space_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("   bab2");
+	ASSERT_EQ(3, actual);
+}
+
+TEST(PasswordTest, nonconsecutive_repeating_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("aba");
+	ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, trailing_repeating_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("bacccc");
+	ASSERT_EQ(1, actual);
+}
+TEST(PasswordTest, shooting_star_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("****");
+	ASSERT_EQ(4, actual);
+}
